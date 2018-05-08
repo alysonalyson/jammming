cb05259969a21929d5026fbb38faee4fc2deda09
Alyson Turley <alyson.turley@gmail.com>
2018-05-07 20:13:20 -0500

    5/7 update 3

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -19,7 +19,9 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+        <input
+          placeholder="Enter A Song, Album, or Artist"
+          onChange={this.handleTermChange} />
         <a>SEARCH</a>
       </div>
     );
