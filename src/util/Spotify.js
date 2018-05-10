007a98aa72601efd94926a5218c5e0f661920c8d
Alyson Turley <alyson.turley@gmail.com>
2018-05-09 19:33:01 -0500

    5/9 update 1

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -17,7 +17,7 @@ const Spotify = {
       window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
-      window.location= `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location= `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&redirect_uri=${redirectURI}`;
     }
   },
 
