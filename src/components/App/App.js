659d4cde668034e37f4cd5ac240f2130dd9afb37
Alyson Turley <alyson.turley@gmail.com>
2018-05-15 19:43:23 -0500

    5/15 update 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -48,8 +48,8 @@ class App extends React.Component {
     this.setState ({
       playlistName: 'New Playlist',
       playlistTracks: []
-    })
-  })
+    }))
+  }
 
   search(term) {
     Spotify.search(term).then(tracks => {
